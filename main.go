2997f7db63cbea52d23eaba8a6c17ad7c6fb6138
Nana Adjei Manu <n.k.a.manu06@gmail.com>
2024-07-02 15:31:09 +0100

    Update main.go
    update comment description

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import (
 )
 
 func main() {
+	// LaunchLever() returns a closure, 
+	// when passed the name of a feature toggle returns true 
+	// when the status of the toggle is on
 	isEnabled := launchlever.LaunchLever("./testdata/test.json")
 	dark_state_enabled := isEnabled("pfx_123")
 	fmt.Println("Hello, 世界", dark_state_enabled)
